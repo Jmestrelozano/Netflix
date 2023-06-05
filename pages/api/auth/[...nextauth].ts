ca86d65936f4c7c714a5ce8b865ce0ebb8f2aed7
Jmestrelozano <jmestrelozano@gmail.com>
2023-06-05 11:29:18 -0500

    authentication with google and github

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,20 +2,20 @@ import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
-// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { compare } from "bcrypt";
 import prismadb from "@/lib/prismadb";
 
 export const authOptions: AuthOptions = {
   providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID || "",
-    //   clientSecret: process.env.GITHUB_SECRET || "",
-    // }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID || "",
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    // }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
     Credentials({
       id: "credentials",
       name: "Credentials",
@@ -30,8 +30,6 @@ export const authOptions: AuthOptions = {
         },
       },
       async authorize(credentials) {
-        console.log({ credentials });
-
         const { email = "", password = "" } = credentials as {
           email: string;
           password: string;
@@ -46,7 +44,6 @@ export const authOptions: AuthOptions = {
             email,
           },
         });
-        console.log({ user }, "aca");
 
         if (!user || !user.hashedPassword) {
           throw new Error("Email does not exist");
@@ -66,7 +63,7 @@ export const authOptions: AuthOptions = {
     signIn: "/auth",
   },
   debug: process.env.NODE_ENV === "development",
-  // adapter: PrismaAdapter(prismadb),
+  adapter: PrismaAdapter(prismadb),
   session: { strategy: "jwt" },
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import axios from "axios";
 import { getSession, signIn } from "next-auth/react";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 import { Input } from "@/components/Input";
 import { useForm } from "@/hooks/useForm";
@@ -107,20 +109,20 @@ const AuthPage = () => {
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
-            {/* <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
-                onClick={undefined}
+                onClick={() => signIn("google", { callbackUrl: "/" })}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
                 <FcGoogle size={32} />
               </div>
               <div
-                onClick={undefined}
+                onClick={() => signIn("github", { callbackUrl: "/" })}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
                 <FaGithub size={32} />
               </div>
-            </div> */}
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix?"
