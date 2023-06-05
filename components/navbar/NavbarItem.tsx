d90820798d4360137219d9db3dad0cf7ec07eed9
Jmestrelozano <jmestrelozano@gmail.com>
2023-06-05 15:41:57 -0500

    create navbar

---
+++ components/navbar/NavbarItem.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface NavbarItemProps {
+  label: string;
+  active?: boolean;
+}
+
+export const NavbarItem: React.FC<NavbarItemProps> = ({ label, active }) => {
+  return (
+    <div
+      className={
+        active
+          ? "text-white cursor-default"
+          : "text-gray-200 hover:text-gray-300 cursor-pointer transition"
+      }
+    >
+      {label}
+    </div>
+  );
+};
