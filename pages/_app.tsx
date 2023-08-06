2a426a493581ced8e9bc2ff21e7be98f5d16364f
Jmestrelozano <jmestrelozano@gmail.com>
2023-08-06 17:45:12 -0500

    remove changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,33 @@
+
+
 import '@/styles/globals.css'
 import '@/styles/netflixIntro.css'
 import 'react-toastify/dist/ReactToastify.css';
 
-
+import { useEffect } from "react"
 import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import router from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>  <ToastContainer />
-    <Component {...pageProps} />
-  </>
+
+
+  const { data: currentUser } = useCurrentUser();
+
+
+  useEffect(() => {
+    if (!currentUser) {
+      router.replace("/auth")
+    }
+  }, [currentUser])
+
+
+
+  return (
+    <>
+      <ToastContainer />
+      <Component {...pageProps} />
+    </>
+  )
 }
